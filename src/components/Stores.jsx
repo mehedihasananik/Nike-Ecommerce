7056c192c134e4ba42ee619d08e6726efdd643a8
marshallanique <marshallanique@gmail.com>
2023-09-10 11:51:14 +0600

    stories added

--- src/components/Stores.jsx
+++ src/components/Stores.jsx
@@ -1,6 +1,24 @@
-const Stores = (props) => {
-  console.log(props);
-  return <div>Enter Stores</div>;
+import News from "./utils/News";
+
+/* eslint-disable react/prop-types */
+
+const Stores = ({ story: { title, news } }) => {
+  return (
+    // main div
+    <div className="container mx-auto">
+      <div className="pb-5 md:pb-10">
+        <h3 className="text-lg md:text-5xl font-bold text-black tracking-wide">
+          {title}
+        </h3>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-5">
+        {news.map((item) => {
+          console.log(item);
+          return <News key={item.id} {...item} />;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Stores;
